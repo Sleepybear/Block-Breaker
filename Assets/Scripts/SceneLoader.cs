9022a2deb2d0aed5cc0e2978a164265410df5ee8
Sleepybear <thomas.ucsc@gmail.com>
2019-11-19 04:24:22 -0800

    revamp sceneloader

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -6,35 +6,11 @@ using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
     [SerializeField] int EndGameSceneIndex;
-    [SerializeField] int FirstLevelIndex;
-    private int currentLevel;
-    private int totalLevels;
-
-    private void Awake()
-    {
-        int loaders = FindObjectsOfType<SceneLoader>().Length;
-        if(loaders >1)
-        {
-            Destroy(gameObject);
-        } else
-        {
-            DontDestroyOnLoad(gameObject);
-        }
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        totalLevels = SceneManager.sceneCount - 2; // Minus two because we don't want to count splash or end game screens
-    }
-
-    public void StartGame()
-    {
-        SceneManager.LoadScene(FirstLevelIndex);
-    }
+    [SerializeField] int NextLevelIndex;
 
     public void LoadNext()
     {
-        SceneManager.LoadScene(++currentLevel % totalLevels);
+         SceneManager.LoadScene(NextLevelIndex);
     }
 
     public void EndGame()
